26725045d633cc23fe9ba368b58d92e3147354cf
micmarsh <mike@marsh.pw>
2015-01-10 23:11:58 -0500

    yay persistent global session

--- modules/clojure/index.js
+++ modules/clojure/index.js
@@ -30,11 +30,19 @@ function buildRequest (expression, session) {
         resolveWithFullResponse: true
     };
     if (!!session) {
-        request.headers = {"Cookie": "ring-session=" + session + ""};
+        request = addCookie(request, "ring-session", session);
     }
     return request;
 }
 
+function addCookie (req, key, value) {
+    var cookie = request.cookie(key + "=" + value);
+    var jar = request.jar();
+    jar.setCookie(cookie, req.uri.split("?")[0]);
+    req.jar = jar;
+    return req;
+}
+ 
 function lookup (key) {
     return function (object) {
         return object[key];
@@ -50,22 +58,25 @@ function saveSession (response) {
         var ringSession = cookie.split(";")[0].split("=")[1];
         globalSession = ringSession;
     } catch (e) {
-        console.error("problem grabbing cookie from response");
-        console.error("headers: ", headers, "cookie: ", cookie);
-        console.error("status: ", response.statusCode, "body: ", response.body);
+        // console.error("problem grabbing cookie from response");
+        // console.error("headers: ", headers, "cookie: ", cookie);
+        // console.error("status: ", response.statusCode, "body: ", response.body);
     }
     return response;
+}
+
+function evalClj (expression) {
+    return request(buildRequest(expression, globalSession)).
+        then(saveSession).
+        then(lookup("body")).
+        then(JSON.parse).
+        then(lookup("result"));
 }
 
 module.exports.msg = function (text, from, reply, raw) {
     if (contains("(", text)) {
         if (balancedParens(text)) {
-            request(buildRequest(text, globalSession))
-                .then(saveSession)
-                .then(lookup("body"))
-                .then(JSON.parse)
-                .then(lookup("result"))
-                .then(reply);
+            evalClj(text).then(reply);
         } else {
             reply("Balance yo parens, homie!");
         }
